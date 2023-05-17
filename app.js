e54c64dfa9b5fde9808592a963cab2494b25d03b
kapanadze04 <kapanadze.gio96@gmail.com>
2023-05-17 17:14:38 +0400

    add some functions

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-const newGameCpu = document.getElementById('new-game-cpu');
-const newGamePlayer = document.getElementById('new-game-player');
-const btnBox = document.querySelectorAll('.btn-box')
-const home = document.getElementById('home');
-const game = document.getElementById('game');
+const choiceButtons = document.querySelectorAll('.btn-box');
 const gameBtns = document.querySelectorAll('.game-btn');
+const home = document.querySelector('#home');
+const game = document.querySelector('#game');
 
-
-let player = "x";
-// let mode = "cpu";
-// let turn = "x";
+let player = 'x';
+let mode = 'cpu';
+let turn = "x";
 let freeButtons = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-// let xArray = [];
-// let oArray = [];
+let xArray = [];
+let oArray = [];
 let winnerCombinations = [
     [0, 1, 2],
     [3, 4, 5],
@@ -23,85 +20,66 @@ let winnerCombinations = [
     [2, 4, 6],
 ]
 
-newGameCpu.addEventListener('click', function() {
-    home.style.display = 'none';
-    game.style.display = 'block';
-})
-
-newGamePlayer.addEventListener('click', function() {
-    home.style.display = 'none';
-    game.style.display = 'block';
-})
-
-
-
-
-function activateBtn(icon){
-    if(icon === "x"){
-        btnBox[0].classList.add("active")
-        btnBox[1].classList.remove("active")
-        player = "x"
+function activateIcon(icon) {
+    if(icon === "x") {
+        choiceButtons[0].classList.add("active");
+        choiceButtons[1].classList.remove("active");
+        player = "x";
     } else {
-        btnBox[1].classList.add("active")
-        btnBox[0].classList.remove("active")
-        player = "o"
+        choiceButtons[1].classList.add("active");
+        choiceButtons[0].classList.remove("active");
+        player = "o";
     }
 }
 
 
+function hoverEffects() {
+    for (let index = 0; index < freeButtons.length; index++) {
+        const gameBtnsIndex = freeButtons[index];
+        if(turn === "x") {
+            gameBtns[gameBtnsIndex].classList.add("xHover");
+            gameBtns[gameBtnsIndex].classList.remove("oHover");
+        } else {
+            gameBtns[gameBtnsIndex].classList.add("oHover");
+            gameBtns[gameBtnsIndex].classList.remove("xHover");
+        }
+    }
+}
 
+function clickedFunction() {
+    for (let index = 0; index < gameBtns.length; index++) {
+        gameBtns[index].onclick = (event) => {
+            event.target.classList.remove("xHover");
+            event.target.classList.remove("oHover");
 
+            const spliceIndex = freeButtons.indexOf(index);
+            freeButtons.splice(spliceIndex, 1);
 
-// function onHoverEffects() {
-//     for (let index = 0; index < freeButtons.length; index++) {
-//         const playButtonIndex = freeButtons[index];
-//         if(turn === "x") {
-//             playButtons[playButtonIndex].classList.add("xHover");
-//             playButtons[playButtonIndex].classList.remove("oHover");
-//         } else {
-//             playButtons[playButtonIndex].classList.add("oHover");
-//             playButtons[playButtonIndex].classList.remove("xHover");
-//         }
+            const icon = document.createElement("img");
+            icon.classList.add("game-icons");
+            
+            if(turn === "x") {
+                icon.src = "./assets/icon-x.svg";
+                event.target.append(icon);
+                turn = "o";
+            } else {
+                icon.src = "./assets/icon-o.svg";
+                event.target.append(icon);
+                turn = "x";
+            }
+
+            hoverEffects();
+
+            event.target.onclick = null;
+        }
         
-//     }
-// }
+    }
+}
 
-// function createClicked() {
-//     for (let index = 0; index < playButtons.length; index++) {
-//         playButtons[index].onclick = (event) => {
-//             event.target.classList.remove("xHover");
-//             event.target.classList.remove("oHover");
-
-
-//             const spliceIndex = freeButtons.indexOf(index);
-//             freeButtons.splice(spliceIndex, 1);
-//             const icon = document.createElement("img");
-//             icon.classList.add("play-icons");
-//             if(turn === "x"){
-//                 icon.src = "./assets/icon-x.svg";
-//                 event.target.append(icon);
-//                 turn = "o";
-                
-//             } else {
-//                 icon.src = "./assets/icon-o.svg";
-//                 event.target.append(icon);
-//                 turn = "x";
-//             }
-//             onHoverEffects();
-
-//             event.target.onclick = null;
-//         }
-        
-//     }
-// }
-
-// function startGame(modeParam) {
-//     home.style.display = "none";
-//     board.style.display = "flex";
-
-//     mode = modeParam;
-//     onHoverEffects();
-//     createClicked();
-// }
-
-
+function startGame(modeParam) {
+    home.style.display = "none";
+    game.style.display = "block";
+    mode = modeParam;
+    hoverEffects();
+    clickedFunction();
+}
